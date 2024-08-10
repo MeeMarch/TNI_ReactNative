a1cd339014eeac6a8c11cf646702572d73ee7998
MeeMarch <rmeenanuch23@gmail.com>
2024-08-10 09:57:42 +0700

    Practice4_TextInput Component

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,16 +5,40 @@ import { stylesLogin } from "../styles/styles";
 const Login = (): React.JSX.Element => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateEmail = (email:string):boolean => {
+    const recheckEmail = /\S+@\S+\.\S+/;
+    return recheckEmail.test(email);
+  }
+
   const handleSubmit = () => {
+    let errorMessage="";
     if (!name) {
-      Alert.alert("Please Enter Name");
+/*       Alert.alert("Alert", "Please Enter Name", [{ text: "OK" }]);
+      return; //break */
+      errorMessage +="Please Enter Name\n";
     }
     if (!email) {
-      Alert.alert("Please Enter Email");
+      /* Alert.alert("Alert", "Please Enter Email", [{ text: "OK" }]);
+      return; */
+      errorMessage +="Please Enter Email\n";
+    }else if(!validateEmail(email)){
+      errorMessage += "Invalid Email Format\n"
     }
-    if (name && email) {
-      Alert.alert("Success");
+
+    //check password
+    if (!password) {
+      errorMessage +="Please Enter Password\n";
+    } else if(password.length<7){
+      errorMessage +="Password must be at least 6 characters\n";
     }
+
+    if(errorMessage){
+      Alert.alert("Error",errorMessage.trim(), [{ text: "OK" }]);
+      return;
+    }
+    Alert.alert("Alert", "Success", [{ text: "OK" }]);
   };
 
   return (
@@ -28,8 +52,16 @@ const Login = (): React.JSX.Element => {
       <TextInput
         style={stylesLogin.input}
         placeholder="Enter Email"
+        keyboardType="email-address"
         value={email}
         onChangeText={setEmail}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
       />
       <Button title="SUBMIT" onPress={handleSubmit} />
     </View>
